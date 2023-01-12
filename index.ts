4d1ba573573267b729cabdcf8f486bbe975335f1
Gregoriiy <gregory.sudakov@btb.dev>
2023-01-12 22:32:25 +0600

    added file manager

--- index.ts
+++ index.ts
@@ -1,41 +1,24 @@
-import { screen , box, table } from 'blessed';
+import { screen , box, table , filemanager} from 'blessed';
 import { readdir } from 'fs/promises';
 
 // Create a screen object.
 var myScreen = screen({
-  smartCSR: true
+  smartCSR: true,
+  dump: __dirname + '/logs/file.log',
+  warnings: true
 })
 async function fire(path: string) : Promise<string[]> {
     return await readdir(path);
 }
 
-
-
-myScreen.title = 'my window title';
-
 // Create a box perfectly centered horizontally and vertically.
-const myMiniBox = box({
-    bottom: 0,
-    right: 0,
-    width: '97%',
-    height: '20%',
-    content: '{center}\n close me!{/center}',
-    tags: true,
-    style: {
-      fg: 'black',
-      bg: 'white',
-     
-      hover: {
-        bg: 'red'
-      }
-    }
-})
-var myBox = box({
+var rightBox = box({
+  parent: myScreen,
   top: 0,
-  left: 0,
-  width: '60%',
+  right: 0,
+  width: '40%',
   height: '100%',
-  content: `{center}\n ${__dirname}{/center}`,
+  content: '{center}commands{/center}',
   tags: true,
   border: {
     type: 'line'
@@ -52,92 +35,67 @@ var myBox = box({
   }
 })
 
-var rightBox = box({
-    top: 0,
+const myMiniBox = box({
+    parent: rightBox,
+    bottom: 0,
     right: 0,
-    width: '40%',
-    height: '100%',
-    content: '{center}commands{/center}',
+    width: '97%',
+    height: '20%',
+    content: '{center}\n close me!{/center}',
     tags: true,
-    border: {
-      type: 'line'
-    },
     style: {
-      fg: 'white',
-      bg: 'magenta',
-      border: {
-        fg: '#f0f0f0'
-      },
+      fg: 'black',
+      bg: 'white',
+     
       hover: {
-        bg: 'green'
+        bg: 'red'
       }
     }
-  })
+})
 
-// Append our box to the screen.
-myScreen.append(myBox);
-rightBox.append(myMiniBox);
-myScreen.append(rightBox);
-
-
-// Add a png icon to the box
-// var icon = image({
-//   parent: myBox,
-//   top: 0,
-//   left: 0,
-//   type: 'overlay',
-//   width: 'shrink',
-//   height: 'shrink',
-//   file: __dirname + '/my-program-icon.png',
-//   search: false
-// });
-
-
-fire(__dirname).then(files => {
-  //myBox.setContent(`{center}${await fire(__dirname)}{/center}`);
-   const myTable =  table({
-        top: 'center',
-        left: 'center',
-        width: '100%',
-        height: '80%',
-        tags: true,
-        border: {
-          type: 'line'
-        },
-        style: {
-          fg: 'white',
-          bg: 'magenta',
-          border: {
-            fg: '#f0f0f0'
-          },
-          hover: {
-            bg: 'green'
-          }
-        }}
-    )
-    myTable.setData(files.map(m => ([m])));
-    myBox.append(myTable);
-    myScreen.render();
+var fm = filemanager({
+  parent: myScreen,
+  border: 'line',
+  style: {
+    selected: {
+      bg: 'blue'
+    }
+  },
+  height: 'half',
+  width: 'half',
+  top: 'center',
+  left: 'center',
+  label: ' {blue-fg}%path{/blue-fg} ',
+  cwd: process.env.HOME,
+  keys: true,
+  vi: true,
+  scrollbar: {
+    ch: ' '
+  }
 });
+
+var yybox = box({
+  style: {
+    bg: 'green'
+  },
+  border: 'line',
+  height: 'half',
+  width: 'half',
+  top: 'center',
+  left: 'center',
+  hidden: true
+});
+
+fm.refresh();
+myScreen.render();
+fm.focus();
+    
+
 myMiniBox.on('click', function() { 
     process.exit(0);
  });
-
-// If box is focused, handle `enter`/`return` and give us some more content.
-myBox.key('enter', function() {
-  myBox.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-  myBox.setLine(1, 'bar');
-  myBox.insertLine(1, 'foo');
-  myScreen.render();
-});
 
 // Quit on Escape, q, or Control-C.
 myScreen.key(['escape', 'q', 'C-c'], function() {
   return process.exit(0);
 });
-
-// Focus our element.
-myBox.focus();
-
-// Render the screen.
-myScreen.render();
